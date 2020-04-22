411cd44f90e50553f3aface98561a18296a7de31
waterbrother2019122132 <1972352623@qq.com>
2020-04-22 21:41:26 +0800

    迭代器实现

--- day9LeetCode.js
+++ day9LeetCode.js
@@ -1,81 +1,127 @@
-//关于列表的实现
 function List() {
-    this.listSize = 0;
-    //this.pos = 0;
+    this.listSize = 0;//列表中元素的个数
+    this.dataStore = [];//初始化一个空数组来保存数据
+    this.clear = clear;//清空列表中的所有元素
+    this.find = find;//在列表中查找某一元素
+    this.toString = toString;//显示列表中的元素
+    this.insert = insert;//向列表中插入一个元素
+    this.append = append;//给列表添加元素
+    this.remove = remove;//从列表中删除元素
+    this.contains = contains;//判断列表中是否存在某一元素
+    this.length = length;//列表中有多少个元素
+    this.pos = 0;//列表当前元素所在位置
+    this.front = front;//将列表当前位置移到第一个位置
+    this.end = end;//将列表当前位置移到最后一个位置
+    this.prev = prev; //将列表当前位置向前移动一个位置
+    this.next = next;//将列表当前位置向后移动一个位置
+    this.currPos = currPos;//列表的当前位置
+    this.moveTo = moveTo;//将列表当前位置移动到某个位置
+    this.getElement = getElement;//获取当前位置的元素
+}
+function clear(){
+    delete this.dataStore;
     this.dataStore = [];
-    // this.clear = clear;
-    this.find = find;
-    this.toString = toString;
-   // this.insert = insert;
-    this.append = append;
-    this.remove = remove;
-    //this.front = front;
-   // this.end = end;
-   // this.prev = prev;
-  //  this.next = next;
-   
-   // this.currPos = currPos;
-  //  this.moveTo = moveTo;
-    //this.getElement = getElement;
-    this.length = length;
- }
- function append(e){
-     this.dataStore[this.listSize++]=e;
- }
- function find(e){
-     for(var i=0;i<this.dataStore.length;++i)
-         if(this.dataStore[i]==e){
-             console.log(i);
+    this.listSize = 0;
+    this.pos = 0;
+}
+
+function find(element) {
+    for (var i = 0; i < this.dataStore.length; ++i) {
+        if (this.dataStore[i] == element) {
             return i;
         }
-            return -1;
- }
- function remove(element) {
-  var foundAt = this.find(element);
-//     if (foundAt > -1)
-//     for(let i=foundAt;i<this.listSize+1;i++){
-//         this.dataStore[i]=this.dataStore[i+1];
-//     }
-//     else return false;
-//     --this.listSize;
-    
-    if (foundAt > -1) {
-       this.dataStore.splice(foundAt,1);
-       --this.listSize;
-       return true;
+    }
+    return -1;
+}
+
+function toString(){
+    return this.dataStore;
+}
+
+function insert(element,after){
+    var insertPos = this.find(after);
+    if(insertPos > -1){
+        this.dataSore.splice(insertPos+1,0,element);
+        this.listSize++;
+        return true;
     }
     return false;
- }
+}
 
+function append(element) {
+    this.dataStore[this.listSize++] = element;
+}
+
+function remove(element) {
+    var foundAt = this.find(element);
+    if (foundAt > -1) {
+        this.dataStore.splice(foundAt,1);
+        this.listSize--;
+        return true;
+    }
+    return false;
+}
+
+function contains(){
+    for(var i= 0; i < this.dataStore.length;i++){
+        if(this.dataStore[i] == element){
+            return true;
+        }
+    }
+    return false;
+}
 
 function length(){
     return this.listSize;
 }
-function toString(){
-    return this.dataStore;
-}
-function insert(element,after){
-    var position=this.find(after);
-    // ++this.listSize;
-    // for(var i=position;i<=this.dataStore.length;i++){
-    //     this.dataStore[i+1]=dataStore[i];
 
-    // }
-    // this.dataStore[position]=e;
-    // this.listSize--;
-    if(position>-1){
-        this.dataStore.splice(position+1,0,element);
-        ++this.listSize;
-        return true;
+function front(){
+    this.pos = 0;
+}
+
+function end(){
+    this.pos = this.listSize-1;
+}
+
+function prev(){
+    if(this.pos > 0){
+        this.pos--;
     }
-    return false;
+}
+
+function next(){
+   if(this.pos < this.listSize-1){
+       this.pos++;
+   }
+}
+
+function currPos(){
+    return this.pos;
+}
+
+function moveTo(position){
+    this.pos = position;
+}
+
+function getElement(){
+    return this.dataStore[this.pos];
 }
 var names = new List();
 names.append("Cynthia");
 names.append("Raymond");
 names.append("Barbara");
-console.log(names.toString());
+names.append("amrbara");
+names.append("zmrbara");
+names.append("wmrbara");
+console.log(names.toString());//[ 'Cynthia', 'Raymond', 'Barbara', 'amrbara', 'zmrbara', 'wmrbara' ]
 names.remove("Raymond");
+console.log(names.toString());//[ 'Cynthia', 'Barbara', 'amrbara', 'zmrbara', 'wmrbara' ]
+names.clear();
 console.log(names.toString());
-names.insert("cy","Barbara");
-console.log(names.toString());
+/*使用迭代器访问列表*/
+for(names.front();names.currPos() <= names.length();names.next()){
+    console.log(names.getElement());
+    if(names.pos == names.length()-1){
+        return false;
+    }
+}
